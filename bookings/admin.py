c525bd6893f7270748c4cc4f1985090247123790
kolliprem <101470592+kolliprem@users.noreply.github.com>
2026-02-26 12:38:44 +0530

    Added Authentication and Permissions (#1)
    Co-authored-by: Prem Kumar <premkumar.kolli@rupyz.com>

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from bookings.models import Booking
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Booking, Service, Offer
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Fields", {"fields": ("role", "phone_number")}),
+    )
+
 
 admin.site.register(Booking)
+admin.site.register(Service)
+admin.site.register(Offer)
